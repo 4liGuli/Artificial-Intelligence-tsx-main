48d512e31bd8d2c938fb0243a87006edf7e6fd4e
4liGuli <AliGuliyev2005@gmail.com>
2024-07-21 16:37:11 +0400

    blog

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,64 +7,64 @@ import 'react-toastify/dist/ReactToastify.css';
 const Contact: React.FC = () => {
     const menimFormum = useRef<HTMLFormElement>(null)
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+      e.preventDefault();
       if(menimFormum.current) {
         emailjs
-        .sendForm('service_trfvdlr', 'template_8p9cg3g', menimFormum.current, {
+        .sendForm('service_trfvdlr', 'template_8p9cg3g', menimFormum.current, 
+          {
           publicKey: 'XZBctI9I3N4K2Wd5Z',
-        })
+          }
+        )
         .then(
           () => {
-            toast.success("Mesaj ugurla gonderildi")
+            toast.success("The message was sent successfully")
           },
           () => {
-            toast.error("Serverde xeta var. Bir az sonra cehd edin.")
+            toast.error("There is an error on the server. Try again later.")
           },
         );
         menimFormum.current.reset()
       }
-      };
+    };
 
   return (
     <div className="contact-form">
-        <div className="container d-flex justify-content-center align-items-center">
-               <form onSubmit={sendEmail} ref={menimFormum}>
-<div className="form-group">
-        <label htmlFor="ad">Name</label >
-        <input
-          type="text"
-          className="form-control shadow-none"
-          id="ad"
-          placeholder="Name"
-          name='adiniz'
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          className="form-control shadow-none"
-         id='email'
-          placeholder="Email"
-          name='epoct'
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="message">Mesajiniz</label>
-        <textarea
-          className="form-control shadow-none"
-         id='message'
-          placeholder="Message"
-          name='mesaj'
-        />
-      </div>
-<div className="d-flex justify-content-center mt-4">
-<button type="submit" className="btn btn-success">
-        Send
-      </button>
-</div>
-    </form>
-        </div> 
+      <div className="container d-flex justify-content-center align-items-center">
+        <form onSubmit={sendEmail} ref={menimFormum}>
+          <div className="form-group">
+            <label htmlFor="ad">Name</label >
+            <input
+              type="text"
+              className="form-control shadow-none"
+              id="ad"
+              placeholder="Name"
+              name='adiniz'
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className="form-control shadow-none"
+              id='email'
+              placeholder="Email"
+              name='epoct'
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Mesajiniz</label>
+            <textarea
+              className="form-control shadow-none"
+              id='message'
+              placeholder="Message"
+              name='mesaj'
+            />
+          </div>
+          <div className="d-flex justify-content-center mt-4">
+            <button type="submit" className="btn btn-success"> Send </button>
+          </div>
+        </form>
+      </div> 
     </div>
   );
 };
